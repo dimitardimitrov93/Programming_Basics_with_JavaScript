152f42ef5391564cfe8cd1e2ae01d33f6eae6949
Dimitar Dimitrov <60186759+dimitardimitrov93@users.noreply.github.com>
2020-04-22 14:46:06 +0300

    Add files via upload

---
+++ Previous Exams/01.Pool-Day/01. Pool Day.js
@@ -0,0 +1,23 @@
+function solve(input) {
+
+    let numberOfPeople = Number(input.shift());
+    let entranceTax = Number(input.shift());
+    let sunbedPrice = Number(input.shift());
+    let umbrellaPrice = Number(input.shift());
+
+    let entranceTaxForAllPeople = numberOfPeople * entranceTax;
+
+    let numberOfSunbeds = Math.ceil(numberOfPeople * 0.75);
+    let moneyForSunbeds = numberOfSunbeds * sunbedPrice;
+
+    let numberOfUmrellas = Math.ceil(numberOfPeople / 2);
+    let priceForUmbrellas = numberOfUmrellas * umbrellaPrice;
+
+    let finalPrice = (entranceTaxForAllPeople + moneyForSunbeds + priceForUmbrellas).toFixed(2);
+
+    console.log(`${finalPrice} lv.`);
+}
+
+solve(21, 5.50, 4.40, 6.20);
+solve(50, 6, 8, 4);
+solve(100, 8, 6, 4);
