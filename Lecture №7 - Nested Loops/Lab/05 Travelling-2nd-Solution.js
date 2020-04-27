eddf30d381398ab711102f7608479f50343c5aa3
Dimitar Dimitrov <60186759+dimitardimitrov93@users.noreply.github.com>
2020-04-27 20:19:33 +0300

    commit

---
+++ Lecture №7 - Nested Loops/Lab/05 Travelling-2nd-Solution.js
@@ -0,0 +1,62 @@
+function solve(input) {
+  let destination = input.shift();
+
+  while (destination != "End") {
+    let minBudget = Number(input.shift());
+    let sum = 0;
+
+    while (sum < minBudget) {
+      let currentAmountMoney = Number(input.shift());
+      sum += currentAmountMoney;
+    }
+
+    console.log(`Going to ${destination}!`);
+    destination = input.shift();
+  }
+}
+
+// solve([
+//   "Greece",
+//   "1000",
+//   "200",
+//   "200",
+//   "300",
+//   "100",
+//   "150",
+//   "240",
+//   "Spain",
+//   "1200",
+//   "300",
+//   "500",
+//   "193",
+//   "423",
+//   "End",
+//   "",
+// ]);
+
+solve([
+  "France",
+  "2000",
+  "300",
+  "300",
+  "200",
+  "400",
+  "190",
+  "258",
+  "360",
+  "Portugal",
+  "1450",
+  "400",
+  "400",
+  "200",
+  "300",
+  "300",
+  "Egypt",
+  "1900",
+  "1000",
+  "280",
+  "300",
+  "500",
+  "End",
+  "",
+]);
