efb9e88cd1e1f593f04cbab0b40498375236940c
Dimitar Dimitrov <60186759+dimitardimitrov93@users.noreply.github.com>
2020-04-28 21:37:28 +0300

    commit

--- Lecture №7 - Nested Loops/Exercise/04. Train The Trainers.js
+++ Lecture №7 - Nested Loops/Exercise/04. Train The Trainers.js
@@ -28,20 +28,20 @@ function solve(input) {
   console.log(`Student's final assessment is ${(finalAssessment / presentationCounter).toFixed(2)}.`);
 }
 
-// solve([
-//   "2",
-//   "While-Loop",
-//   "6.00",
-//   "5.50",
-//   "For-Loop",
-//   "5.84",
-//   "5.66",
-//   "Finish",
-// ]);
 solve([
-  '3',    'Arrays',
-  '4.53', '5.23',
-  '5.00', 'Lists',
-  '5.83', '6.00',
-  '5.42', 'Finish'
+  "2",
+  "While-Loop",
+  "6.00",
+  "5.50",
+  "For-Loop",
+  "5.84",
+  "5.66",
+  "Finish",
 ]);
+// solve([
+//   '3',    'Arrays',
+//   '4.53', '5.23',
+//   '5.00', 'Lists',
+//   '5.83', '6.00',
+//   '5.42', 'Finish'
+// ]);
