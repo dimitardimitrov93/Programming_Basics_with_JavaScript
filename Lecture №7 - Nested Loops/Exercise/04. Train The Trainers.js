8e1f1c7e9bc5e91ee48bb3997e319354c2f3d867
Dimitar Dimitrov <60186759+dimitardimitrov93@users.noreply.github.com>
2020-04-28 21:36:40 +0300

    commit

---
+++ Lecture №7 - Nested Loops/Exercise/04. Train The Trainers.js
@@ -0,0 +1,47 @@
+function solve(input) {
+  let juryNum = Number(input.shift());
+  let command = input.shift();
+  let finalAssessment = 0;
+  let presentationCounter = 0;
+
+  while (command != "Finish") {
+    let presentationName = command;
+
+    let averageGrade = 0;
+    let sum = 0;
+
+    for (let i = 0; i < juryNum; i++) {
+      let grade = Number(input.shift());
+      sum += grade;
+    }
+
+    averageGrade = sum / juryNum;
+    finalAssessment += averageGrade;
+
+    console.log(`${presentationName} - ${averageGrade.toFixed(2)}.`);
+
+    presentationCounter++;
+
+    command = input.shift();
+  }
+
+  console.log(`Student's final assessment is ${(finalAssessment / presentationCounter).toFixed(2)}.`);
+}
+
+// solve([
+//   "2",
+//   "While-Loop",
+//   "6.00",
+//   "5.50",
+//   "For-Loop",
+//   "5.84",
+//   "5.66",
+//   "Finish",
+// ]);
+solve([
+  '3',    'Arrays',
+  '4.53', '5.23',
+  '5.00', 'Lists',
+  '5.83', '6.00',
+  '5.42', 'Finish'
+]);
